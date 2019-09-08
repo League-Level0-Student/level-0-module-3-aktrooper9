858a9a1daa344a520bd5caaeccf9f3b7160e707d
League Student <league@iMac-20.attlocal.net>
2019-09-08 13:00:29 -0700

    IP HighLow

--- src/_02_crazy_cat_lady/CrazyCatLady.java
+++ src/_02_crazy_cat_lady/CrazyCatLady.java
@@ -5,18 +5,26 @@ package _02_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
-		// 1. Ask the user how many cats they have
-
+	// 1. Ask the user how many cats they have
+String t1= JOptionPane.showInputDialog("How many cats do you have?"); 
 		// 2. Convert their answer into an int
-
+		int t2 = Integer.parseInt(t1);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-
+ if(t2==(3)) {
+	 JOptionPane.showInputDialog("you are crazy about cats");
+ }
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-
+ else if(t2==(2)) { 
+	 playVideo("https://www.youtube.com/watch?v=Z1umOMRqTW0");
+ }
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
+ else  { 
+	 playVideo("https://www.youtube.com/watch?v=Z1umOMRqTW0");
+ }
 	}
 
 	static void playVideo(String videoURL) {
@@ -27,6 +35,7 @@ public class CrazyCatLady {
 			e.printStackTrace();
 		}
 	}
+	}
 
-}
+
 
--- src/_03_high_low/HighLowGame.java
+++ src/_03_high_low/HighLowGame.java
@@ -5,25 +5,33 @@ package _03_high_low;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		int random = new Random().nextInt(99)+1;
 		
 		// 2. Print out the random variable above
-		
+			System.out.println(random);
 		// 11. Repeat steps 1 to 10 ten times
 		
 			// 1. Ask the user for a guess using a pop-up window, and save their response 
-
+String t1 = JOptionPane.showInputDialog("choose a numberfrom 1 to 100");
 			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-			
+		int t2 = Integer.parseInt(t1);	
 			// 5. if the guess is correct
-				// 6. Win
+			if(t2==(random)) {
+				JOptionPane.showInputDialog("good job you have won the video game");
+			}	
+			// 6. Win
 				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
 			// 7. if the guess is high
-				// 8. Tell them it's too high
+			else if(t2>random) {
+				JOptionPane.showInputDialog("To high");
+			}	
+			// 8. Tell them it's too high
 			// 9. if the guess is low
 				// 10. Tell them it's too low
 
