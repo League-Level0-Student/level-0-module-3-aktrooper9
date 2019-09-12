4e4adde2a40cbe3e7a427f2d6cece61d0fb9bdd8
League Student <league@iMac-18.attlocal.net>
2019-09-11 18:27:36 -0700

    AreYoueHappy ip

--- src/_03_high_low/HighLowGame.java
+++ src/_03_high_low/HighLowGame.java
@@ -11,26 +11,38 @@ public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
+		
+		
 		int random = new Random().nextInt(99)+1;
 		
 		// 2. Print out the random variable above
-			System.out.println(random);
+			//System.out.println(random);
 		// 11. Repeat steps 1 to 10 ten times
-		
+			for(int i = 0;i<10;i+=1) {
 			// 1. Ask the user for a guess using a pop-up window, and save their response 
 String t1 = JOptionPane.showInputDialog("choose a numberfrom 1 to 100");
 			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-		int t2 = Integer.parseInt(t1);	
+		 int t2 = Integer.parseInt(t1);	
 			// 5. if the guess is correct
-			if(t2==(random)) {
-				JOptionPane.showInputDialog("good job you have won the video game");
-			}	
+		 
+		 if(t2==(random)) {
+				JOptionPane.showMessageDialog(null,"good job you have won the video game");
+		System.exit(0);
+		 }	
 			// 6. Win
 				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
 			// 7. if the guess is high
 			else if(t2>random) {
-				JOptionPane.showInputDialog("To high");
+				JOptionPane.showMessageDialog(null,"Too high");
 			}	
+			else if(t2<random) {
+				JOptionPane.showMessageDialog(null,"Too low");
+			}	
+		}
+			
+			JOptionPane.showMessageDialog(null,"You lose the number was "+random+" okay");
+			
+			// 
 			// 8. Tell them it's too high
 			// 9. if the guess is low
 				// 10. Tell them it's too low
---
+++ src/_04_are_you_happy/Are_You_Happy.java
@@ -0,0 +1,66 @@
+package _04_are_you_happy;
+
+import javax.swing.JOptionPane;
+
+public class Are_You_Happy {
+	public static void main(String[] args) {
+		String t =JOptionPane.showInputDialog("Are you happy?");
+		if(t.equalsIgnoreCase("yes")) {
+			JOptionPane.showMessageDialog(null, "Keep Doing what you're doing.");
+		}
+		else if(t.equalsIgnoreCase("no")) {
+			String t1 =JOptionPane.showInputDialog("Do you want to be happy ==}==========================+");
+		
+			if(t1.equalsIgnoreCase("no")) {
+				//String
+			}
+			else if(t1.equalsIgnoreCase("yes")) {
+			JOptionPane.showInputDialog(null, "Keep doing what you're doing");
+			}			
+	
+		
+			}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
